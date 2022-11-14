ad6ca0772c0a1e0306cafe847811178b52a8405a
ahtaum <aditalmahdi@gmail.com>
2022-11-15 06:42:35 +0700

    add CORS and LOGGING

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.19
 require github.com/go-playground/validator/v10 v10.11.1
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+)
+
+require (
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
--- server.go
+++ server.go
@@ -3,12 +3,25 @@ package main
 import (
 	"go_temp/db"
 	"go_temp/routes"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	db.Init()
 
 	e := routes.Init()
+
+	// LOGGING
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:8080", "http://localhost:8080"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
